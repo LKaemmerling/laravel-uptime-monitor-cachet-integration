d152db08916d880f2269d63f11be523627e0c0e7
Lukas Kämmerling <lukas.kaemmerling@s-imk.de>
2016-11-25 22:37:16 +0100

    Add Typehint suggestion from insight and remove readme code

--- README.md
+++ README.md
@@ -4,8 +4,3 @@ Cachet Integration Package for Laravel Uptime Monitor http://lk-development.de/
 
 # WORK IN PROGRESS
 This Package isn't stable yet. You can try it, but only at your own risk.
-
-# Installation
-```bash
-composer
-```
--- src/Listener/LaravelUptimeMonitorEventSubscriber.php
+++ src/Listener/LaravelUptimeMonitorEventSubscriber.php
@@ -3,6 +3,7 @@
 namespace LKDevelopment\UptimeMonitorCachetIntegration\Listener;
 
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use LKDevelopment\UptimeMonitorCachetIntegration\Enums\Incident;
 use LKDevelopment\UptimeMonitorCachetIntegration\Helpers\CachetApiHelper;
 
@@ -16,7 +17,7 @@ class LaravelUptimeMonitorEventSubscriber
      */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(config('laravel-uptime-monitor-cachet-integration.issueableEvents'), function ($event) {
+        $events->listen(config('laravel-uptime-monitor-cachet-integration.issueableEvents'), function (ShouldQueue $event) {
             $helper = new CachetApiHelper();
             $helper->sendIncident(Incident::parse($event->monitor, class_basename($event)));
         });

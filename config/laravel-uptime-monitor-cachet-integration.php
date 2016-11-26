b91fa889fdf4e59a8a1e7c37f8dd4d6fda3215a9
LKDevelopment <kontakt@lukas-kaemmerling.de>
2016-11-26 14:01:26 +0000

    Apply fixes from StyleCI

--- config/laravel-uptime-monitor-cachet-integration.php
+++ config/laravel-uptime-monitor-cachet-integration.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'endpoint' => env('CACHET_ENDPOINT','https://demo.cachethq.io/api/v1/'),
-    'token' => env('CACHET_TOKEN','ChangeMe'),
+    'endpoint' => env('CACHET_ENDPOINT', 'https://demo.cachethq.io/api/v1/'),
+    'token' => env('CACHET_TOKEN', 'ChangeMe'),
     'enable' => true,
     'issueableEvents' => [
         Spatie\UptimeMonitor\Events\UptimeCheckFailed::class,
